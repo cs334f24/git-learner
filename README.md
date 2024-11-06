49323cce65fd90250e6194b01a2b03d93a14460b
JP Appel <jeanpierre.appel01@gmail.com>
2024-11-06 14:15:42 -0500

    Initial Project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Git Learner
+
+A system for practicing various workflows associated with Git and GitHub
---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello_world():
+        return "<p>Hello World</p>"
+
+    return app
+
+
+if __name__ == "__main__":
+    create_app().run()
