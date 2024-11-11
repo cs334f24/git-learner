8b1c00c94de46f6cff836a4c677040ac88c3fbaa
JP Appel <jeanpierre.appel01@gmail.com>
2024-11-11 11:44:37 -0500

    Add simple index and start db work

--- app/app.py
+++ app/app.py
@@ -1,15 +1,38 @@
-from flask import Flask
+from authlib.integrations.flask_client import OAuth
+from flask import Flask, render_template
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
 
+    oauth = OAuth(app)
+
+    oauth.register(
+        name="github",
+        client_id="",
+        client_secret="",
+        access_token_url="",
+        authorize_url="",
+    )
+
     @app.route("/")
-    def hello_world():
-        return "<p>Hello World</p>"
+    def index():
+        return render_template("index.html")
+
+    @app.route("/login")
+    def login():
+        return ""
+
+    @app.route("/authorize")
+    def authorize():
+        return ""
+
+    @app.route("/logout")
+    def logout():
+        return ""
 
     return app
 
 
 if __name__ == "__main__":
-    create_app().run()
+    create_app().run(port=8080)
