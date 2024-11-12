33b6a0b34da7e308757496155a43d8142894f97f
JP Appel <jeanpierre.appel01@gmail.com>
2024-11-12 16:54:55 -0500

    Add individual module routes

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Git Learner
 
 A system for practicing various workflows associated with Git and GitHub
+
+## Notes
+
+`Repository.get_clones_traffic` can be polled to check if a user has cloned the repo
--- app/modules.py
+++ app/modules.py
@@ -1,19 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 from .auth import login_required
 
 bp = Blueprint("modules", __name__)
 
+@bp.route("/modules")
+def modules_home():
+    modules = [
+        {"id": i, "name": f"Module {i+1}", "step": 2 * i, "total_steps": 3 * i + 1}
+        for i in range(5)
+    ]
+    return render_template('modules_home.html', modules=modules)
 
 @login_required
-@bp.route("/module/<module_id>")
-def module_home(module_id: str):
+@bp.route("/modules/<module_id>")
+def module_page(module_id: str):
     print("loading module:", module_id)
     return f"<h1>Loading Module {module_id} ...</h1>"
 
 
 @login_required
-@bp.route("/module/<module_id>/step/<int:module_step>")
+@bp.route("/modules/<module_id>/step/<int:module_step>")
 def module_setp(module_id: str, module_step: int):
     print(f"On Module {module_id} step {module_step}")
     return f"<h1>Module {module_id}</h1><h2>Step {module_step}</h2>"
