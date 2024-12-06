6f1cad4b26a6cd8bb82f2b8e3608c61987e61e0b
JP Appel <jeanpierre.appel01@gmail.com>
2024-12-06 11:15:31 -0500

    Finish module

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,11 @@
-from .basic import module as basic_module
+from module_core.steps import Module
 
-active_modules = {"basic module": basic_module}
+from .basic import module as basic_module
+from .clone_commit_update_push import module as push_after_update_module
+
+active_modules: dict[str, Module] = {
+    "basic module": basic_module,
+    "push-after-update": push_after_update_module,
+}
 
 __all__ = ["basic_module", "active_modules"]
